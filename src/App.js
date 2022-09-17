aad4881ebfcbe7b24e3e01b31d02f8b46ce13fb1
yoonzytech <dev.rubyc@gmail.com>
2022-09-18 02:11:47 +0800

    category slider working

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 
 import Header from "./components/Header.js";
 // import Body from "./components/Body";
-import Slider  from "./components/Slider.js";
+import Slider from "./components/Slider";
 
 function App() {
   
   return (
     <div className="App">
       <Header />
+      {/* <Body /> */}
       <Slider />
     </div>
   );
